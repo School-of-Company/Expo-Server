e150e4bf1b2ae537c2a37e2c51ff99dbeafc1367
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-08 09:48:58 +0900

    init

---
+++ src/main/java/java/team/startup/expo/ExpoApplication.java
@@ -0,0 +1,13 @@
+package java.team.startup.expo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/team/startup/expo/ExpoApplicationTests.java
@@ -0,0 +1,13 @@
+package java.team.startup.expo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
