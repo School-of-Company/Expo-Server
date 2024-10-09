fc4962c90069cfb27648885dbccd5f6a1a3a3af4
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-09 19:16:35 +0900

    add :: RequestLogFilter 추가

---
+++ src/main/java/team/startup/expo/global/filter/RequestLogFilter.java
@@ -0,0 +1,43 @@
+package team.startup.expo.global.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+public class RequestLogFilter extends OncePerRequestFilter {
+
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        log.info("=========================");
+
+        String[] logMessages = {
+                "client ip = " + request.getRemoteAddr(),
+                "request method = " + request.getMethod(),
+                "request url = " + request.getRequestURI(),
+                "client info = " + request.getHeader("User-Agent")
+        };
+
+        for (String message : logMessages) {
+            log.info(message);
+        }
+
+        log.info("=========================");
+
+        try {
+            filterChain.doFilter(request, response);
+            log.info("=========================");
+            log.info("response status = " + response.getStatus());
+            log.info("=========================");
+        } catch (Exception e) {
+            log.error("=========================");
+            log.error(e.getCause().toString());
+            log.error(e.getMessage());
+            log.error(e.getCause().getMessage());
+            log.error("=========================");
+        }
+    }
+}
+
+
