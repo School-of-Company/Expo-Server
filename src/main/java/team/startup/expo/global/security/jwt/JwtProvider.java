711614323580a3b549504cba682160827af40df4
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-09 19:16:51 +0900

    add :: JwtProvider 추가

---
+++ src/main/java/team/startup/expo/global/security/jwt/JwtProvider.java
@@ -0,0 +1,66 @@
+package team.startup.expo.global.security.jwt;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import team.startup.expo.global.security.exception.ExpiredTokenException;
+import team.startup.expo.global.security.exception.InvalidTokenException;
+import java.util.Date;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+    private static final String AUTHORITIES_KEY = "auth";
+    private static final long ACCESS_TOKEN_TIME = 60L * 15 * 4;
+    public static final long REFRESH_TOKEN_TIME = 60L * 60 * 24 * 7;
+
+    private final JwtProperties jwtProperties;
+
+    private Key getSignInAccessKey() {
+        return Keys.hmacShaKeyFor(jwtProperties.getAccessSecret().getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Key getSignInSecretKey() {
+        return Keys.hmacShaKeyFor(jwtProperties.getRefreshSecret().getBytes(StandardCharsets.UTF_8));
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSignInAccessKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            throw new  ExpiredTokenException();
+        } catch (Exception e) {
+            throw new InvalidTokenException();
+        }
+    }
+
+    public String generateAccessToken(String email) {
+        Date accessTokenExpiresIn = new Date(System.currentTimeMillis() + ACCESS_TOKEN_TIME*1000);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim(AUTHORITIES_KEY, "JWT")
+                .setIssuedAt(new Date())
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSignInAccessKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email) {
+        Date refreshTokenExpiresIn = new Date(System.currentTimeMillis() + REFRESH_TOKEN_TIME*1000);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(refreshTokenExpiresIn)
+                .signWith(getSignInSecretKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
