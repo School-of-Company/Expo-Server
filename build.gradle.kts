dd28fc2666a89b3002241b78241088d0f29644a7
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-09 19:14:40 +0900

    add :: jwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation ("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation ("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.withType<Test> {
