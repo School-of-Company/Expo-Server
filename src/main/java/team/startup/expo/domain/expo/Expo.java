f9da7fbb3cc23aca76225971b844df9a43ba5103
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-17 14:14:05 +0900

    add :: Expo 추가

---
+++ src/main/java/team/startup/expo/domain/expo/Expo.java
@@ -0,0 +1,48 @@
+package team.startup.expo.domain.expo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import team.startup.expo.domain.admin.Admin;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Expo {
+
+    @Id
+    @GeneratedValue(generator = "ulidGenerator")
+    @GenericGenerator(name = "ulidGenerator", strategy = "team.startup.expo.global.common.ulid.ULIDGenerator")
+    @Column(nullable = false, unique = true, columnDefinition = "BINARY(16)")
+    private byte[] id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
+    private String startedDay;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
+    private String finishedDay;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
+    private String startedTime;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
+    private String finishedTime;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String location;
+
+    @ManyToOne
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+}
