62d2edecf866d80a11337f7c5cd8073f3168939e
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-17 14:15:52 +0900

    add :: TraineeProgramUser 추가

---
+++ src/main/java/team/startup/expo/domain/training/TrainingProgramUser.java
@@ -0,0 +1,34 @@
+package team.startup.expo.domain.training;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import team.startup.expo.domain.trainee.Trainee;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class TrainingProgramUser {
+
+    @Id
+    @GeneratedValue(generator = "ulidGenerator")
+    @GenericGenerator(name = "ulidGenerator", strategy = "team.startup.expo.global.common.ulid.ULIDGenerator")
+    @Column(nullable = false, unique = true, columnDefinition = "BINARY(16)")
+    private byte[] id;
+
+    @Column(nullable = false)
+    private Boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "trainingPro_id")
+    private TrainingProgram trainingProgram;
+
+    @ManyToOne
+    @JoinColumn(name = "trainee_id")
+    private Trainee trainee;
+}
