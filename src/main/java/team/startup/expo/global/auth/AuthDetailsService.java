f9f5877d36f43e22f15ded0c777c8355c679065a
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-17 14:13:52 +0900

    add :: User -> Admin 추가

--- src/main/java/team/startup/expo/global/auth/AuthDetailsService.java
+++ src/main/java/team/startup/expo/global/auth/AuthDetailsService.java
@@ -6,17 +6,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import team.startup.expo.domain.user.exception.NotFoundUserException;
-import team.startup.expo.domain.user.repository.UserRepository;
+import team.startup.expo.domain.admin.exception.NotFoundUserException;
+import team.startup.expo.domain.admin.repository.AdminRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AuthDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
+    private final AdminRepository adminRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
+        return adminRepository.findByEmail(email)
                 .map(AuthDetails::new)
                 .orElseThrow(NotFoundUserException::new);
     }
