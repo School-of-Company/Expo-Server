086dae7a545a36432bebc19b49b91094864a7e35
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-17 14:13:31 +0900

    add :: AdminRepository 추가

---
+++ src/main/java/team/startup/expo/domain/admin/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package team.startup.expo.domain.admin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import team.startup.expo.domain.admin.Admin;
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByEmail(String email);
+}
