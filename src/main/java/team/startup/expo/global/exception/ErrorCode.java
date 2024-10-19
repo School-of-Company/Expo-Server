07495923ba06c01dc5557e3dd444be71e76ce395
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-20 04:15:14 +0900

    add :: NotVerificationSmsException

--- src/main/java/team/startup/expo/global/exception/ErrorCode.java
+++ src/main/java/team/startup/expo/global/exception/ErrorCode.java
@@ -11,6 +11,8 @@ public enum ErrorCode {
     // sms
     DUPLICATE_PHONE_NUMBER(409, "이미 존재하는 전화번호입니다."),
     NOT_FOUND_SMS_AUTH(404, "찾을 수 없는 sms입니다."),
+    NOT_VERIFICATION_SMS(401, "인증되지 않은 sms입니다."),
+
     // token
     EXPIRED_TOKEN(401, "토큰이 만료되었습니다."),
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
