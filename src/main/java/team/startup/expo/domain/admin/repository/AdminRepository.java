2860e2b33271b9ccc0421b9da62d98c123b9790d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-22 11:57:22 +0900

    add :: existsByEmail 추가

--- src/main/java/team/startup/expo/domain/admin/repository/AdminRepository.java
+++ src/main/java/team/startup/expo/domain/admin/repository/AdminRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByEmail(String email);
     Boolean existsByPhoneNumber(String phoneNumber);
+    Boolean existsByEmail(String email);
     Boolean existsByNickname(String nickname);
 }
