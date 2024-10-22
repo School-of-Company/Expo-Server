c4b36e3d481638f3b4712dd6d270a4765ee7426e
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-22 15:47:16 +0900

    add :: Logout api 추가

--- src/main/java/team/startup/expo/domain/auth/presentation/AuthController.java
+++ src/main/java/team/startup/expo/domain/auth/presentation/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import team.startup.expo.domain.auth.presentation.dto.request.SignInRequestDto;
 import team.startup.expo.domain.auth.presentation.dto.request.SignUpRequestDto;
 import team.startup.expo.domain.auth.presentation.dto.response.TokenResponseDto;
+import team.startup.expo.domain.auth.service.LogoutService;
 import team.startup.expo.domain.auth.service.ReissueTokenService;
 import team.startup.expo.domain.auth.service.SignInService;
 import team.startup.expo.domain.auth.service.SignUpService;
@@ -19,6 +20,7 @@ public class AuthController {
     private final SignUpService signUpService;
     private final SignInService signInService;
     private final ReissueTokenService reissueTokenService;
+    private final LogoutService logoutService;
 
     @PostMapping
     public ResponseEntity<Void> signUp(@RequestBody SignUpRequestDto dto) {
@@ -36,5 +38,11 @@ public class AuthController {
     public ResponseEntity<TokenResponseDto> reissueToken(@RequestHeader("RefreshToken") String refreshToken) {
         TokenResponseDto result = reissueTokenService.execute(refreshToken);
         return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> logout() {
+        logoutService.execute();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
