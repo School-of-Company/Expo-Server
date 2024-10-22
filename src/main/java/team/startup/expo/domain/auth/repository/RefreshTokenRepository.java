341195534344c7d0600d8ef809e2fe650c30b935
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-22 13:59:19 +0900

    add :: findByToken 추가

--- src/main/java/team/startup/expo/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/team/startup/expo/domain/auth/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package team.startup.expo.domain.auth.repository;
 import org.springframework.data.repository.CrudRepository;
 import team.startup.expo.global.util.count.RefreshToken;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String refreshToken);
 }
