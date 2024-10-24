44ee3bbbe46b26b6a7fbfc4616abea8086c29279
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-24 23:18:17 +0900

    add :: findByTrainingProgram 메서드 추가

--- src/main/java/team/startup/expo/domain/training/repository/TrainingProgramUserRepository.java
+++ src/main/java/team/startup/expo/domain/training/repository/TrainingProgramUserRepository.java
@@ -2,10 +2,12 @@ package team.startup.expo.domain.training.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.trainee.Trainee;
+import team.startup.expo.domain.training.TrainingProgram;
 import team.startup.expo.domain.training.TrainingProgramUser;
 
 import java.util.List;
 
 public interface TrainingProgramUserRepository extends JpaRepository<TrainingProgramUser, Long> {
     List<TrainingProgramUser> findByTrainee(Trainee traine);
+    List<TrainingProgramUser> findByTrainingProgram(TrainingProgram trainingProgram);
 }
