f81cf9b392a3882ef02b6f07fdc07f79bec745f3
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-24 18:09:16 +0900

    add :: generateExpo url 추가

--- src/main/java/team/startup/expo/global/security/config/SecurityConfig.java
+++ src/main/java/team/startup/expo/global/security/config/SecurityConfig.java
@@ -76,6 +76,9 @@ public class SecurityConfig {
                                 // trainee
                                 .requestMatchers(HttpMethod.GET, "/trainee").hasAnyAuthority(Authority.ROLE_ADMIN.name())
 
+                                // expo
+                                .requestMatchers(HttpMethod.POST, "/expo").hasAnyAuthority(Authority.ROLE_ADMIN.name())
+
                                 .anyRequest().denyAll()
                 )
 
