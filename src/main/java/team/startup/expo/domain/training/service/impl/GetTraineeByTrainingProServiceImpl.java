70d2506aa3d3b24158c08b485866db32e3a68180
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-24 23:17:41 +0900

    add :: GetTraineeByTrainingPro api 추가

---
+++ src/main/java/team/startup/expo/domain/training/service/impl/GetTraineeByTrainingProServiceImpl.java
@@ -0,0 +1,37 @@
+package team.startup.expo.domain.training.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import team.startup.expo.domain.training.TrainingProgram;
+import team.startup.expo.domain.training.exception.NotFoundTrainingProgramException;
+import team.startup.expo.domain.training.presentation.dto.response.GetTrainingProTraineeResponseDto;
+import team.startup.expo.domain.training.repository.TrainingProgramRepository;
+import team.startup.expo.domain.training.repository.TrainingProgramUserRepository;
+import team.startup.expo.domain.training.service.GetTraineeByTrainingProService;
+import team.startup.expo.global.annotation.ReadOnlyTransactionService;
+
+import java.util.List;
+
+@ReadOnlyTransactionService
+@RequiredArgsConstructor
+public class GetTraineeByTrainingProServiceImpl implements GetTraineeByTrainingProService {
+
+    private final TrainingProgramUserRepository trainingProgramUserRepository;
+    private final TrainingProgramRepository trainingProgramRepository;
+
+    public List<GetTrainingProTraineeResponseDto> execute(Long trainingProId) {
+        TrainingProgram trainingProgram = trainingProgramRepository.findById(trainingProId)
+                .orElseThrow(NotFoundTrainingProgramException::new);
+
+        return trainingProgramUserRepository.findByTrainingProgram(trainingProgram).stream()
+                .map(trainingProgramUser -> GetTrainingProTraineeResponseDto.builder()
+                        .name(trainingProgramUser.getTrainee().getName())
+                        .organization(trainingProgramUser.getTrainee().getOrganization())
+                        .position(trainingProgramUser.getTrainee().getPosition())
+                        .programName(trainingProgramUser.getTrainingProgram().getTitle())
+                        .status(trainingProgramUser.getStatus())
+                        .entryTime(trainingProgramUser.getEntryTime())
+                        .leaveTime(trainingProgramUser.getLeaveTime())
+                        .build())
+                .toList();
+    }
+}
