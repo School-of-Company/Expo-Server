f816ee9ad403502376243818ce248fc234921075
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-05 10:09:48 +0900

    add :: ParticipantRepository 추가

---
+++ src/main/java/team/startup/expo/domain/participant/repository/ParticipantRepository.java
@@ -0,0 +1,10 @@
+package team.startup.expo.domain.participant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.startup.expo.domain.participant.ExpoParticipant;
+
+import java.util.Optional;
+
+public interface ParticipantRepository extends JpaRepository<ExpoParticipant, Long> {
+    Optional<ExpoParticipant> findByPhoneNumber(String phoneNumber);
+}
