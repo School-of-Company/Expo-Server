cffd4228141a83d059945b1b14c980e94e303c3f
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-06 16:47:33 +0900

    add :: WebMvcConfig

---
+++ src/main/java/team/startup/expo/global/security/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package team.startup.expo.global.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
