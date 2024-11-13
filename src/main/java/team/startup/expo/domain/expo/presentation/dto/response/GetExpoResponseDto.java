1ec4d0170a9050c54a20754311db12874f153f91
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-14 00:25:26 +0900

    add :: response에 id 추가

--- src/main/java/team/startup/expo/domain/expo/presentation/dto/response/GetExpoResponseDto.java
+++ src/main/java/team/startup/expo/domain/expo/presentation/dto/response/GetExpoResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class GetExpoResponseDto {
+    private Long id;
     private String title;
     private String description;
     private String startedDay;
