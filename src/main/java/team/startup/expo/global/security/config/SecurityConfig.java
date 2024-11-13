91f01c9f1fdbce8cbd3cb65d1c37bdea8b81c74e
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-13 16:10:13 +0900

    add :: url 추가

--- src/main/java/team/startup/expo/global/security/config/SecurityConfig.java
+++ src/main/java/team/startup/expo/global/security/config/SecurityConfig.java
@@ -82,6 +82,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/training/{expo_id}").hasAnyAuthority(Authority.ROLE_ADMIN.name())
                                 .requestMatchers(HttpMethod.PATCH, "/training/{trainingPro_id}").hasAnyAuthority(Authority.ROLE_ADMIN.name())
                                 .requestMatchers(HttpMethod.DELETE, "/training/{trainingPro_id}").hasAnyAuthority(Authority.ROLE_ADMIN.name())
+                                .requestMatchers(HttpMethod.GET, "/training/program/{expo_id}").hasAnyAuthority(Authority.ROLE_ADMIN.name())
 
                                 // expo
                                 .requestMatchers(HttpMethod.POST, "/expo").hasAnyAuthority(Authority.ROLE_ADMIN.name())
