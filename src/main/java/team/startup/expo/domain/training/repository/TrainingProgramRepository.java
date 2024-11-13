e911e834f56002be0415d7f7b0effaec7fa99f23
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-13 16:10:25 +0900

    add :: findByExpo 추가

--- src/main/java/team/startup/expo/domain/training/repository/TrainingProgramRepository.java
+++ src/main/java/team/startup/expo/domain/training/repository/TrainingProgramRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.expo.Expo;
 import team.startup.expo.domain.training.TrainingProgram;
 
+import java.util.List;
+
 public interface TrainingProgramRepository extends JpaRepository<TrainingProgram, Long> {
     void deleteByExpo(Expo expo);
+    List<TrainingProgram> findByExpo(Expo expo);
 }
