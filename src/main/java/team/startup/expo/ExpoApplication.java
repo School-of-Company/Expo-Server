6e25f3cbda10d819b28fa894ef59a8687594a898
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-20 15:43:07 +0900

    add :: TimeZone 설정 추가

--- src/main/java/team/startup/expo/ExpoApplication.java
+++ src/main/java/team/startup/expo/ExpoApplication.java
@@ -1,12 +1,20 @@
 package team.startup.expo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ExpoApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpoApplication.class, args);
