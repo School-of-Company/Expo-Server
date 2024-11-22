85acfa3b01a835e0b468f51e5fc1854721bf4a7a
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-22 17:21:46 +0900

    add :: String -> LocalDateTime

--- src/main/java/team/startup/expo/domain/standard/presentation/dto/request/AddStandardProRequestDto.java
+++ src/main/java/team/startup/expo/domain/standard/presentation/dto/request/AddStandardProRequestDto.java
@@ -1,9 +1,12 @@
 package team.startup.expo.domain.standard.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
@@ -16,8 +19,10 @@ public class AddStandardProRequestDto {
     @NotNull
     @Size(max = 20)
     private String startedAt;
+    private LocalDateTime startedAt;
 
     @NotNull
     @Size(max = 20)
     private String endedAt;
+    private LocalDateTime endedAt;
 }
