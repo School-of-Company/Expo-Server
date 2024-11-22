4884e423f2ef3f6cf1afdebcf6d1d6e81eeb4269
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-22 17:21:24 +0900

    add :: addStandardProList api 추가

--- src/main/java/team/startup/expo/domain/standard/presentation/StandardController.java
+++ src/main/java/team/startup/expo/domain/standard/presentation/StandardController.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team.startup.expo.domain.standard.presentation.dto.request.AddStandardProRequestDto;
+import team.startup.expo.domain.standard.service.AddStandardProListService;
 import team.startup.expo.domain.standard.service.AddStandardProService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,10 +16,17 @@ import team.startup.expo.domain.standard.service.AddStandardProService;
 public class StandardController {
 
     private final AddStandardProService addStandardProService;
+    private final AddStandardProListService addStandardProListService;
 
     @PostMapping("/{expo_id}")
     public ResponseEntity<Void> addStandardPro(@PathVariable("expo_id") Long expoId, @RequestBody AddStandardProRequestDto dto) {
         addStandardProService.execute(expoId, dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/list/{expo_id}")
+    public ResponseEntity<Void> addStandardProList(@PathVariable("expo_id") Long expoId, @RequestBody List<AddStandardProRequestDto> dtos) {
+        addStandardProListService.execute(expoId, dtos);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
---
+++ src/main/java/team/startup/expo/domain/standard/service/impl/AddStandardProListServiceImpl.java
@@ -0,0 +1,39 @@
+package team.startup.expo.domain.standard.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import team.startup.expo.domain.expo.Expo;
+import team.startup.expo.domain.expo.exception.NotFoundExpoException;
+import team.startup.expo.domain.expo.repository.ExpoRepository;
+import team.startup.expo.domain.standard.StandardProgram;
+import team.startup.expo.domain.standard.presentation.dto.request.AddStandardProRequestDto;
+import team.startup.expo.domain.standard.repository.StandardProgramRepository;
+import team.startup.expo.domain.standard.service.AddStandardProListService;
+import team.startup.expo.global.annotation.TransactionService;
+
+import java.util.List;
+
+@TransactionService
+@RequiredArgsConstructor
+public class AddStandardProListServiceImpl implements AddStandardProListService {
+
+    private final StandardProgramRepository standardProgramRepository;
+    private final ExpoRepository expoRepository;
+
+    public void execute(Long expoId, List<AddStandardProRequestDto> dtos) {
+        Expo expo = expoRepository.findById(expoId)
+                .orElseThrow(NotFoundExpoException::new);
+
+        dtos.forEach(dto -> saveStandardProgram(dto, expo));
+    }
+
+    private void saveStandardProgram(AddStandardProRequestDto dto, Expo expo) {
+        StandardProgram standardProgram = StandardProgram.builder()
+                .title(dto.getTitle())
+                .startedAt(String.valueOf(dto.getStartedAt()))
+                .endedAt(String.valueOf(dto.getEndedAt()))
+                .expo(expo)
+                .build();
+
+        standardProgramRepository.save(standardProgram);
+    }
+}
