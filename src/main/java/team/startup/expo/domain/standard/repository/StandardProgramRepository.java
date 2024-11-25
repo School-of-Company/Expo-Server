d1adbd62e933b2f763af99cee5c3783f30977dbf
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-25 18:19:56 +0900

    add :: findByExpo 추가

--- src/main/java/team/startup/expo/domain/standard/repository/StandardProgramRepository.java
+++ src/main/java/team/startup/expo/domain/standard/repository/StandardProgramRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.expo.Expo;
 import team.startup.expo.domain.standard.StandardProgram;
 
+import java.util.List;
+
 public interface StandardProgramRepository extends JpaRepository<StandardProgram, Long> {
     void deleteByExpo(Expo expo);
+    List<StandardProgram> findByExpo(Expo expo);
 }
