2c471e178c9368f668b770aebd37658115c69400
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-25 19:56:16 +0900

    add :: findByStandardProgram 추가

--- src/main/java/team/startup/expo/domain/standard/repository/StandardProgramUserRepository.java
+++ src/main/java/team/startup/expo/domain/standard/repository/StandardProgramUserRepository.java
@@ -5,10 +5,12 @@ import team.startup.expo.domain.participant.ExpoParticipant;
 import team.startup.expo.domain.standard.StandardProgram;
 import team.startup.expo.domain.standard.StandardProgramUser;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StandardProgramUserRepository extends JpaRepository<StandardProgramUser, Long> {
     Optional<StandardProgramUser> findByStandardProgramAndExpoParticipant(StandardProgram standardProgram, ExpoParticipant expoParticipant);
     Boolean existsByExpoParticipantAndStandardProgram(ExpoParticipant expoParticipant, StandardProgram standardProgram);
     void deleteByStandardProgram(StandardProgram standardProgram);
+    List<StandardProgramUser> findByStandardProgram(StandardProgram standardProgram);
 }
