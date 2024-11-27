963748ceea38113210776a1cb03854c168d82582
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-27 14:18:52 +0900

    add :: PreEnterScanQrCodeResponseDto

---
+++ src/main/java/team/startup/expo/domain/attendance/presentation/dto/response/PreEnterScanQrCodeResponseDto.java
@@ -0,0 +1,12 @@
+package team.startup.expo.domain.attendance.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PreEnterScanQrCodeResponseDto {
+    private String name;
+    private String affiliation;
+    private byte[] qrCode;
+}
