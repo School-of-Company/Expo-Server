62d6cc0bf0652f75f283e2a69fe1545ab388a0d2
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-29 10:31:51 +0900

    update :: response 값 수정

--- src/main/java/team/startup/expo/domain/trainee/presentation/dto/response/GetTraineeInformationResponseDto.java
+++ src/main/java/team/startup/expo/domain/trainee/presentation/dto/response/GetTraineeInformationResponseDto.java
@@ -2,14 +2,21 @@ package team.startup.expo.domain.trainee.presentation.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import team.startup.expo.domain.trainee.ParticipationType;
 
 import java.util.List;
 
 @Getter
 @Builder
 public class GetTraineeInformationResponseDto {
+    private Long id;
     private String name;
-    private List<GetTrainingProgramResponseDto> trainingProgram;
     private String trainingId;
     private Boolean laptopStatus;
+    private String phoneNumber;
+    private String position;
+    private String schoolLevel;
+    private String organization;
+    private Boolean informationStatus;
+    private ParticipationType participationType;
 }
