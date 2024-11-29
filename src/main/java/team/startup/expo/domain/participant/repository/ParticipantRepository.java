a47fd7b1d89b226b0913ed16550ca50a8e9ecead
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-29 10:01:12 +0900

    add :: findByExpoAndParticipationType 쿼리 메서드 추가

--- src/main/java/team/startup/expo/domain/participant/repository/ParticipantRepository.java
+++ src/main/java/team/startup/expo/domain/participant/repository/ParticipantRepository.java
@@ -3,10 +3,13 @@ package team.startup.expo.domain.participant.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.expo.Expo;
 import team.startup.expo.domain.participant.ExpoParticipant;
+import team.startup.expo.domain.trainee.ParticipationType;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ParticipantRepository extends JpaRepository<ExpoParticipant, Long> {
     Optional<ExpoParticipant> findByPhoneNumber(String phoneNumber);
     void deleteByExpo(Expo expo);
+    List<ExpoParticipant> findByExpoAndParticipationType(Expo expo, ParticipationType participationType);
 }
