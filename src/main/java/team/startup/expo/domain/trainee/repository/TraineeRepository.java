f8c7c6d468a40030babef7d4f6dd7707ae2d159e
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-29 10:37:15 +0900

    add :: findByExpo 추가

--- src/main/java/team/startup/expo/domain/trainee/repository/TraineeRepository.java
+++ src/main/java/team/startup/expo/domain/trainee/repository/TraineeRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.expo.Expo;
 import team.startup.expo.domain.trainee.Trainee;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TraineeRepository extends JpaRepository<Trainee, Long> {
     Optional<Trainee> findByPhoneNumber(String phoneNumber);
-    Optional<Trainee> findByTrainingId(String trainingId);
+    List<Trainee> findByExpo(Expo expo);
     void deleteByExpo(Expo expo);
 }
