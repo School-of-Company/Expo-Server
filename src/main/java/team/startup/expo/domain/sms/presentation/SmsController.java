c1843be7135f17c4afb0c913671bce2794c0606a
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-12-24 12:26:57 +0900

    add :: SendMessage api 추가

--- src/main/java/team/startup/expo/domain/sms/presentation/SmsController.java
+++ src/main/java/team/startup/expo/domain/sms/presentation/SmsController.java
@@ -1,12 +1,15 @@
 package team.startup.expo.domain.sms.presentation;
 
 import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.message.response.MultipleDetailMessageSentResponse;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import team.startup.expo.domain.sms.presentation.dto.request.SendMessageRequestDto;
 import team.startup.expo.domain.sms.presentation.dto.request.SendQrRequestDto;
 import team.startup.expo.domain.sms.presentation.dto.request.SendSmsRequestDto;
+import team.startup.expo.domain.sms.service.SendMessageService;
 import team.startup.expo.domain.sms.service.SendQrService;
 import team.startup.expo.domain.sms.service.SendSmsService;
 import team.startup.expo.domain.sms.service.VerifySmsService;
@@ -19,6 +22,7 @@ public class SmsController {
     private final SendSmsService sendSmsService;
     private final VerifySmsService verifySmsService;
     private final SendQrService sendQrService;
+    private final SendMessageService sendMessageService;
 
     @PostMapping
     public ResponseEntity<SingleMessageSentResponse> sendSms(@RequestBody SendSmsRequestDto dto) {
@@ -35,6 +39,12 @@ public class SmsController {
     @PostMapping("/qr")
     public ResponseEntity<SingleMessageSentResponse> sendQr(@RequestBody SendQrRequestDto dto) {
         SingleMessageSentResponse response = sendQrService.execute(dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/message/{expo_id}")
+    public ResponseEntity<MultipleDetailMessageSentResponse> sendMessage(@PathVariable("expo_id") String expoId, @RequestBody SendMessageRequestDto dto) {
+        MultipleDetailMessageSentResponse response = sendMessageService.execute(expoId, dto);
         return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/team/startup/expo/domain/sms/service/impl/SendMessageServiceImpl.java
@@ -0,0 +1,103 @@
+package team.startup.expo.domain.sms.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.response.MultipleDetailMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import team.startup.expo.domain.expo.Expo;
+import team.startup.expo.domain.expo.exception.NotExistParticipantAtExpoException;
+import team.startup.expo.domain.expo.exception.NotExistTraineeAtExpoException;
+import team.startup.expo.domain.expo.exception.NotFoundExpoException;
+import team.startup.expo.domain.expo.repository.ExpoRepository;
+import team.startup.expo.domain.participant.ExpoParticipant;
+import team.startup.expo.domain.participant.repository.ParticipantRepository;
+import team.startup.expo.domain.sms.presentation.dto.request.SendMessageRequestDto;
+import team.startup.expo.domain.sms.service.SendMessageService;
+import team.startup.expo.domain.trainee.Trainee;
+import team.startup.expo.domain.trainee.repository.TraineeRepository;
+import team.startup.expo.global.annotation.ReadOnlyTransactionService;
+import team.startup.expo.global.sms.SmsProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ReadOnlyTransactionService
+@RequiredArgsConstructor
+public class SendMessageServiceImpl implements SendMessageService {
+
+    private final DefaultMessageService messageService;
+    private final SmsProperties smsProperties;
+    private final TraineeRepository traineeRepository;
+    private final ParticipantRepository participantRepository;
+    private final ExpoRepository expoRepository;
+
+    public MultipleDetailMessageSentResponse execute(String expoId, SendMessageRequestDto dto) {
+        Expo expo = expoRepository.findById(expoId)
+                .orElseThrow(NotFoundExpoException::new);
+
+        MultipleDetailMessageSentResponse response = new MultipleDetailMessageSentResponse();
+
+        switch (dto.getAuthority()) {
+            case ROLE_STANDARD -> response = sendMessageForParticipant(expo, dto);
+            case ROLE_TRAINEE -> response = sendMessageForTrainee(expo, dto);
+        }
+
+        return response;
+    }
+
+    private MultipleDetailMessageSentResponse sendMessageForTrainee(Expo expo, SendMessageRequestDto dto) {
+        List<Trainee> traineeList = traineeRepository.findByExpo(expo);
+
+        if (traineeList.isEmpty())
+            throw new NotExistParticipantAtExpoException();
+
+        ArrayList<Message> messageList = new ArrayList<>();
+
+        for (Trainee trainee : traineeList) {
+            Message message = new Message();
+            message.setFrom(smsProperties.getFromNumber());
+            message.setTo(trainee.getPhoneNumber());
+            message.setText(dto.getTitle() + "\n" + dto.getContent());
+
+            messageList.add(message);
+        }
+
+        return sendMessage(messageList);
+    }
+
+    private MultipleDetailMessageSentResponse sendMessageForParticipant(Expo expo, SendMessageRequestDto dto) {
+        List<ExpoParticipant> participantList = participantRepository.findByExpo(expo);
+
+        if (participantList.isEmpty())
+            throw new NotExistTraineeAtExpoException();
+
+        ArrayList<Message> messageList = new ArrayList<>();
+
+        for (ExpoParticipant participant : participantList) {
+            Message message = new Message();
+            message.setFrom(smsProperties.getFromNumber());
+            message.setTo(participant.getPhoneNumber());
+            message.setText(dto.getTitle() + "\n" + dto.getContent());
+
+            messageList.add(message);
+        }
+
+        return sendMessage(messageList);
+    }
+
+    private MultipleDetailMessageSentResponse sendMessage(ArrayList<Message> messageList) {
+        MultipleDetailMessageSentResponse response = new MultipleDetailMessageSentResponse();
+
+        try {
+            response = messageService.send(messageList, false, true);
+        } catch (NurigoMessageNotReceivedException exception) {
+            System.out.println(exception.getFailedMessageList());
+            System.out.println(exception.getMessage());
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return response;
+    }
+}
