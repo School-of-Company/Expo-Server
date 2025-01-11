c679db6d98f821cc9ab99d3083fa7d674ed055dc
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-12 02:52:36 +0900

    add :: StandardParticipantParticipation 추가

---
+++ src/main/java/team/startup/expo/domain/participant/entity/StandardParticipantParticipation.java
@@ -0,0 +1,35 @@
+package team.startup.expo.domain.participant.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class StandardParticipantParticipation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime entryTime;
+
+    @Column(nullable = false)
+    private LocalDateTime leaveTime;
+
+    @Column(nullable = false)
+    private LocalDate attendanceDate;
+
+    @JoinColumn(name = "standard_participant_id")
+    @ManyToOne
+    private StandardParticipant standardParticipant;
+}
