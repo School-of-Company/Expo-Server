8cc312e0799dcb51d916ac17e10cbf17e9507270
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-12 02:07:54 +0900

    add :: DynamicForm 추가

---
+++ src/main/java/team/startup/expo/domain/form/entity/DynamicForm.java
@@ -0,0 +1,35 @@
+package team.startup.expo.domain.form.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.startup.expo.domain.expo.Expo;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class DynamicForm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(50)")
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    @Lob
+    private String jsonData;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FormType formType;
+
+    @JoinColumn(name = "expo_id")
+    @ManyToOne
+    private Expo expo;
+}
