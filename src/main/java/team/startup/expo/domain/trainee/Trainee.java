eacb46cbc310ddd26430c2a1e2d18d8f883b53df
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-12 02:14:41 +0900

    update :: ER-D에 맞게 엔티티 변경

--- src/main/java/team/startup/expo/domain/trainee/Trainee.java
+++ src/main/java/team/startup/expo/domain/trainee/Trainee.java
@@ -19,33 +19,21 @@ public class Trainee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
-    private String trainingId;
-
-    @Column(nullable = false, columnDefinition = "TINYINT(1)")
-    private Boolean laptopStatus;
+    @Column(nullable = false, columnDefinition = "VARCHAR(10)")
+    private String name;
 
     @Column(nullable = false, columnDefinition = "VARCHAR(15)")
     private String phoneNumber;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String informationJson;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Authority authority;
 
-    @Column(nullable = false, columnDefinition = "VARCHAR(10)")
-    private String position;
-
-    @Column(nullable = false, columnDefinition = "VARCHAR(10)")
-    private String schoolLevel;
-
-    @Column(nullable = false, columnDefinition = "VARCHAR(30)")
-    private String organization;
-
-    @Column(nullable = false, columnDefinition = "VARCHAR(10)")
-    private String name;
-
     @Column(nullable = false, columnDefinition = "TINYINT(1)")
-    private Boolean informationStatus;
+    private Boolean personalInformationStatus;
 
     @Column(nullable = false, columnDefinition = "TINYINT(1)")
     private Boolean attendanceStatus = false;
