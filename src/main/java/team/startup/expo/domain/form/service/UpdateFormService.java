6a772d9a74123747f02417a03e59dbc38704ec18
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-14 17:37:50 +0900

    add :: UpdateForm api 추가

---
+++ src/main/java/team/startup/expo/domain/form/service/UpdateFormService.java
@@ -0,0 +1,7 @@
+package team.startup.expo.domain.form.service;
+
+import team.startup.expo.domain.form.presentation.dto.request.FormRequestDto;
+
+public interface UpdateFormService {
+    void execute(Long formId, FormRequestDto dto);
+}
