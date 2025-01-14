bc7256e215fb622a02e2b3d726c50e4a67b29680
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-14 17:36:20 +0900

    add :: findByForm 쿼리 메서드 추가

--- src/main/java/team/startup/expo/domain/form/repository/DynamicFormRepository.java
+++ src/main/java/team/startup/expo/domain/form/repository/DynamicFormRepository.java
@@ -2,6 +2,10 @@ package team.startup.expo.domain.form.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.form.entity.DynamicForm;
+import team.startup.expo.domain.form.entity.Form;
+
+import java.util.List;
 
 public interface DynamicFormRepository extends JpaRepository<DynamicForm, Long> {
+    List<DynamicForm> findByForm(Form form);
 }
