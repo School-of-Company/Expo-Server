372cd5419b132ba1905cc9bb5f16ee2f6e9294f2
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-25 02:11:39 +0900

    update :: PROJECT_ROOT 변경

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
+PROJECT_ROOT="/home/ubuntu/.ssh/Expo-Server"
 JAR_FILE="$PROJECT_ROOT/expo-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
