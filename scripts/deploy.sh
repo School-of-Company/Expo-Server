dbbd7ddda6f7982b3c1ab6352cc4e3c10e2128cd
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-25 15:19:28 +0900

    update :: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,6 @@ JAR_PATH=$REPOSITORY/$PROJECT_NAME/build/libs/expo-0.0.1-SNAPSHOT.jar
 
 cd $REPOSITORY/$PROJECT_NAME/scripts
 
-chmod +x deploy.sh
-sudo chmod +x /home/ubuntu/app/scripts/deploy.sh
-
 CURRENT_PID=$(lsof -ti tcp:8080)
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
