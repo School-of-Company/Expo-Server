fd8fd3fb1627fe71028bbe728866980c7a8191af
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-27 14:33:28 +0900

    add :: Expo 연관관계 매핑

--- src/main/java/team/startup/expo/domain/participant/entity/StandardParticipantParticipation.java
+++ src/main/java/team/startup/expo/domain/participant/entity/StandardParticipantParticipation.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.startup.expo.domain.expo.entity.Expo;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -33,4 +34,12 @@ public class StandardParticipantParticipation {
     @JoinColumn(name = "standard_participant_id")
     @ManyToOne
     private StandardParticipant standardParticipant;
+
+    @JoinColumn(name = "expo_id")
+    @ManyToOne
+    private Expo expo;
+
+    public void addLeaveTime() {
+        this.leaveTime = LocalDateTime.now();
+    }
 }
