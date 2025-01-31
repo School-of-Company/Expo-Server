4f620424c2b7efeebdd9225d4f28ecd32e030d91
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-01-31 15:25:26 +0900

    add :: JPQL을 사용하여 직접 Query 추가

--- src/main/java/team/startup/expo/domain/trainee/repository/TraineeRepository.java
+++ src/main/java/team/startup/expo/domain/trainee/repository/TraineeRepository.java
@@ -1,6 +1,7 @@
 package team.startup.expo.domain.trainee.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import team.startup.expo.domain.expo.entity.Expo;
 import team.startup.expo.domain.trainee.entity.Trainee;
 
@@ -10,6 +11,10 @@ import java.util.Optional;
 public interface TraineeRepository extends JpaRepository<Trainee, Long> {
     Optional<Trainee> findByPhoneNumberAndExpo(String phoneNumber, Expo expo);
     List<Trainee> findByExpo(Expo expo);
+    @Query("SELECT t FROM Trainee t " +
+            "JOIN t.expo e " +
+            "WHERE e = :expo " +
+            "AND t.name LIKE %:name%")
     List<Trainee> findByExpoAndName(Expo expo, String name);
     void deleteByExpo(Expo expo);
     Boolean existsByPhoneNumberAndExpo(String phoneNumber, Expo expo);
