4b734d8ec9a69011a6757dbb7899959da86ebc9b
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-02 06:07:45 +0900

    add :: findBySurvey 쿼리메서드 추가

--- src/main/java/team/startup/expo/domain/survey/repository/DynamicSurveyRepository.java
+++ src/main/java/team/startup/expo/domain/survey/repository/DynamicSurveyRepository.java
@@ -2,6 +2,10 @@ package team.startup.expo.domain.survey.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.survey.entity.DynamicSurvey;
+import team.startup.expo.domain.survey.entity.Survey;
+
+import java.util.List;
 
 public interface DynamicSurveyRepository extends JpaRepository<DynamicSurvey, Long> {
+    List<DynamicSurvey> findBySurvey(Survey survey);
 }
