9f83b096f4ac0ffaa887bf82d933cf1b52fdc55d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-02 06:26:17 +0900

    add :: deleteBySurvey 쿼리메서드 추가

--- src/main/java/team/startup/expo/domain/survey/repository/DynamicSurveyRepository.java
+++ src/main/java/team/startup/expo/domain/survey/repository/DynamicSurveyRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface DynamicSurveyRepository extends JpaRepository<DynamicSurvey, Long> {
     List<DynamicSurvey> findBySurvey(Survey survey);
+    void deleteBySurvey(Survey survey);
 }
