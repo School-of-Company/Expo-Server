48f57376ce94b4bfb51d7a7393403e74bc7b326b
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-02 05:14:33 +0900

    add :: SurveyRepository 추가

---
+++ src/main/java/team/startup/expo/domain/survey/repository/SurveyRepository.java
@@ -0,0 +1,10 @@
+package team.startup.expo.domain.survey.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.startup.expo.domain.expo.entity.Expo;
+import team.startup.expo.domain.form.entity.ParticipationType;
+import team.startup.expo.domain.survey.entity.Survey;
+
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+    Boolean existsByExpoAndParticipationType(Expo expo, ParticipationType participationType);
+}
