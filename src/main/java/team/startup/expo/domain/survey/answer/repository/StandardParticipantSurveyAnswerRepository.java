79de47d911e0a00379f21e70f6580807dcf28a32
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-02 17:31:20 +0900

    add :: StandardParticipantSurveyAnswerRepository 추가

---
+++ src/main/java/team/startup/expo/domain/survey/answer/repository/StandardParticipantSurveyAnswerRepository.java
@@ -0,0 +1,7 @@
+package team.startup.expo.domain.survey.answer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.startup.expo.domain.survey.answer.entity.StandardParticipantSurveyAnswer;
+
+public interface StandardParticipantSurveyAnswerRepository extends JpaRepository<StandardParticipantSurveyAnswer, Long> {
+}
