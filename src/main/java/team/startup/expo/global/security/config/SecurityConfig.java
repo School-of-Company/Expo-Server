6b2badbfac3ac61118a46a4fa02f25caf543cda2
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-02 17:31:56 +0900

    add :: url 추가

--- src/main/java/team/startup/expo/global/security/config/SecurityConfig.java
+++ src/main/java/team/startup/expo/global/security/config/SecurityConfig.java
@@ -140,6 +140,9 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.PATCH, "/survey/{expo_id}").hasAnyAuthority(Authority.ROLE_ADMIN.name())
                                 .requestMatchers(HttpMethod.DELETE, "/survey/{expo_id}").hasAnyAuthority(Authority.ROLE_ADMIN.name())
 
+                                // surveyAnswer
+                                .requestMatchers(HttpMethod.POST, "/survey/answer/standard/{expo_id}").permitAll()
+
                                 //image
                                 .requestMatchers(HttpMethod.POST, "/image").authenticated()
 
