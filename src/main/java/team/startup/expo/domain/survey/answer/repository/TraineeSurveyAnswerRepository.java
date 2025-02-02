221b22ba4aab66ef54573f05fda467f20476ae18
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-02 17:45:05 +0900

    add :: existsByTrainee 쿼리메서드 추가

--- src/main/java/team/startup/expo/domain/survey/answer/repository/TraineeSurveyAnswerRepository.java
+++ src/main/java/team/startup/expo/domain/survey/answer/repository/TraineeSurveyAnswerRepository.java
@@ -2,6 +2,8 @@ package team.startup.expo.domain.survey.answer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.survey.answer.entity.TraineeSurveyAnswer;
+import team.startup.expo.domain.trainee.entity.Trainee;
 
 public interface TraineeSurveyAnswerRepository extends JpaRepository<TraineeSurveyAnswer, Long> {
+    Boolean existsByTrainee(Trainee trainee);
 }
