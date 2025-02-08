391a876b388aa5d8d012ba8da76fc587e292a1e4
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-08 15:41:18 +0900

    setting :: CORS 허용

--- src/main/java/team/startup/expo/global/security/config/WebMvcConfig.java
+++ src/main/java/team/startup/expo/global/security/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://school-expo.netlify.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
