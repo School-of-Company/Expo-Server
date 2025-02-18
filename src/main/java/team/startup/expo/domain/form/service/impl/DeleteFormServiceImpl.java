f28feddda50283a251ed702f1161c2e70b2628d9
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-18 12:49:14 +0900

    add :: Path에 participationType 추가

--- src/main/java/team/startup/expo/domain/form/service/impl/DeleteFormServiceImpl.java
+++ src/main/java/team/startup/expo/domain/form/service/impl/DeleteFormServiceImpl.java
@@ -1,7 +1,11 @@
 package team.startup.expo.domain.form.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import team.startup.expo.domain.expo.entity.Expo;
+import team.startup.expo.domain.expo.exception.NotFoundExpoException;
+import team.startup.expo.domain.expo.repository.ExpoRepository;
 import team.startup.expo.domain.form.entity.Form;
+import team.startup.expo.domain.form.entity.ParticipationType;
 import team.startup.expo.domain.form.exception.NotFoundFormException;
 import team.startup.expo.domain.form.repository.DynamicFormRepository;
 import team.startup.expo.domain.form.repository.FormRepository;
@@ -12,11 +16,15 @@ import team.startup.expo.global.annotation.TransactionService;
 @RequiredArgsConstructor
 public class DeleteFormServiceImpl implements DeleteFormService {
 
+    private final ExpoRepository expoRepository;
     private final FormRepository formRepository;
     private final DynamicFormRepository dynamicFormRepository;
 
-    public void execute(Long formId) {
-        Form form = formRepository.findById(formId)
+    public void execute(String expoId, ParticipationType participationType) {
+        Expo expo = expoRepository.findById(expoId)
+                .orElseThrow(NotFoundExpoException::new);
+
+        Form form = formRepository.findByExpoAndParticipationType(expo, participationType)
                 .orElseThrow(NotFoundFormException::new);
 
         dynamicFormRepository.deleteByForm(form);
