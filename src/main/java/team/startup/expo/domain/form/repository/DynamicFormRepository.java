08345a2a48fee904a27629de428e7d25ebcac5e2
김승찬 <s24041@gsm.hs.kr>
2025-03-18 23:10:04 +0900

    delete: 관련 메서드 제거

--- src/main/java/team/startup/expo/domain/form/repository/DynamicFormRepository.java
+++ src/main/java/team/startup/expo/domain/form/repository/DynamicFormRepository.java
@@ -1,7 +1,6 @@
 package team.startup.expo.domain.form.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import team.startup.expo.domain.expo.entity.Expo;
 import team.startup.expo.domain.form.entity.DynamicForm;
 import team.startup.expo.domain.form.entity.Form;
 
@@ -10,5 +9,4 @@ import java.util.List;
 public interface DynamicFormRepository extends JpaRepository<DynamicForm, Long> {
     void deleteByForm(Form form);
     List<DynamicForm> findByForm(Form form);
-    boolean existsByExpo(Expo expo);
 }
