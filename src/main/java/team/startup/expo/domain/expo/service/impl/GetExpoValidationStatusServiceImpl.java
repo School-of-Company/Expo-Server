06411a3ad93554f905fec6f48b7c1b4fa9dcc8ce
김승찬 <s24041@gsm.hs.kr>
2025-03-18 23:13:48 +0900

    refactor: dynamic 조회 전 존재 여부를 확인하도록 변경

--- src/main/java/team/startup/expo/domain/expo/service/impl/GetExpoValidationStatusServiceImpl.java
+++ src/main/java/team/startup/expo/domain/expo/service/impl/GetExpoValidationStatusServiceImpl.java
@@ -6,7 +6,7 @@ import team.startup.expo.domain.expo.exception.NotFoundExpoException;
 import team.startup.expo.domain.expo.presentation.dto.response.GetExpoValidationStatusResponseDto;
 import team.startup.expo.domain.expo.repository.ExpoRepository;
 import team.startup.expo.domain.expo.service.GetExpoValidationStatusService;
-import team.startup.expo.domain.form.repository.DynamicFormRepository;
+import team.startup.expo.domain.form.repository.FormRepository;
 import team.startup.expo.domain.survey.management.repository.SurveyRepository;
 import team.startup.expo.global.annotation.ReadOnlyTransactionService;
 
@@ -15,18 +15,18 @@ import team.startup.expo.global.annotation.ReadOnlyTransactionService;
 public class GetExpoValidationStatusServiceImpl implements GetExpoValidationStatusService {
 
     private final ExpoRepository expoRepository;
-    private final DynamicFormRepository dynamicFormRepository;
+    private final FormRepository formRepository;
     private final SurveyRepository surveyRepository;
 
     public GetExpoValidationStatusResponseDto execute(String expoId) {
         Expo expo = expoRepository.findById(expoId)
                 .orElseThrow(NotFoundExpoException::new);
 
-        boolean dynamicFormExists = dynamicFormRepository.existsByExpo(expo);
+        boolean formExists = formRepository.existsByExpo(expo);
         boolean surveyExists = surveyRepository.existsByExpo(expo);
 
         return GetExpoValidationStatusResponseDto.builder()
-                .dynamicFormCreatedStatus(dynamicFormExists)
+                .dynamicFormCreatedStatus(formExists)
                 .surveyCreatedStatus(surveyExists)
                 .build();
     }
