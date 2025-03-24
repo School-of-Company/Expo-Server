fe4cb28daa7cf6a82e4f3eaf0c3dfbe6dc231b3e
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-03-24 16:12:01 +0900

    add :: cache 적용

--- src/main/java/team/startup/expo/domain/form/service/impl/UpdateFormServiceImpl.java
+++ src/main/java/team/startup/expo/domain/form/service/impl/UpdateFormServiceImpl.java
@@ -1,6 +1,8 @@
 package team.startup.expo.domain.form.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import team.startup.expo.domain.expo.entity.Expo;
 import team.startup.expo.domain.expo.repository.ExpoRepository;
 import team.startup.expo.domain.form.entity.DynamicForm;
@@ -14,12 +16,14 @@ import team.startup.expo.global.annotation.TransactionService;
 
 @TransactionService
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = "Form")
 public class UpdateFormServiceImpl implements UpdateFormService {
 
     private final ExpoRepository expoRepository;
     private final FormRepository formRepository;
     private final DynamicFormRepository dynamicFormRepository;
 
+    @CacheEvict(key = "#expoId + '_' + #dto.participantType")
     public void execute(String expoId, FormRequestDto dto) {
         Expo expo = expoRepository.findById(expoId)
                 .orElseThrow(NotFoundFormException::new);
