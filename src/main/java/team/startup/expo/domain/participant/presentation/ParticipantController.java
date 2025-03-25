8c3a64d1cefac4bd3121491faea69ea28bb7fbb9
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-03-25 17:48:44 +0900

    add :: ParticipantController 에 @RequestParam 추가

--- src/main/java/team/startup/expo/domain/participant/presentation/ParticipantController.java
+++ src/main/java/team/startup/expo/domain/participant/presentation/ParticipantController.java
@@ -1,12 +1,16 @@
 package team.startup.expo.domain.participant.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team.startup.expo.domain.participant.presentation.dto.response.GetParticipantInfoResponseDto;
 import team.startup.expo.domain.participant.service.GetParticipantInfoService;
 import team.startup.expo.domain.trainee.entity.ApplicationType;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -17,12 +21,17 @@ public class ParticipantController {
     private final GetParticipantInfoService getParticipantInfoService;
 
     @GetMapping("/{expo_id}")
-    public ResponseEntity<List<GetParticipantInfoResponseDto>> getParticipantInfo(
+    public ResponseEntity<Page<GetParticipantInfoResponseDto>> getParticipantInfo(
             @PathVariable("expo_id") String expoId,
             @RequestParam(value = "type") ApplicationType type,
-            @RequestParam(value = "name", required = false) String name
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "size", defaultValue = "7") int size,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
     ) {
-        List<GetParticipantInfoResponseDto> response = getParticipantInfoService.execute(expoId, type, name);
+        PageRequest pageable = PageRequest.of(page, size);
+
+        Page<GetParticipantInfoResponseDto> response = getParticipantInfoService.execute(expoId, type, name, pageable, date);
         return ResponseEntity.ok(response);
     }
 }
