853131fee16a5fc83f71869fd6a8245f5924726f
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-09 20:38:48 +0900

    update :: PreEnterScanQrCode API response 변경

--- src/main/java/team/startup/expo/domain/attendance/presentation/dto/response/PreEnterScanQrCodeResponseDto.java
+++ src/main/java/team/startup/expo/domain/attendance/presentation/dto/response/PreEnterScanQrCodeResponseDto.java
@@ -2,11 +2,14 @@ package team.startup.expo.domain.attendance.presentation.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import team.startup.expo.domain.form.entity.ParticipationType;
 
 @Getter
 @Builder
 public class PreEnterScanQrCodeResponseDto {
+    private Long id;
     private String name;
     private String phoneNumber;
     private Boolean personalInformationStatus;
+    private ParticipationType participationType;
 }
