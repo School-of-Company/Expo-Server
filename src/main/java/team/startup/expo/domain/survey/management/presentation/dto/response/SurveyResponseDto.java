fd44e96ec269e00ad91fcdf56cb92ec1418e1f3c
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-10 08:43:05 +0900

    update :: getSurvey api response informationText field

--- src/main/java/team/startup/expo/domain/survey/management/presentation/dto/response/SurveyResponseDto.java
+++ src/main/java/team/startup/expo/domain/survey/management/presentation/dto/response/SurveyResponseDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class SurveyResponseDto {
+    private String informationText;
     private ParticipationType participationType;
     private List<DynamicSurveyResponseDto> dynamicSurveyResponseDto;
 
