87406881a26193c6dd2ad0b5f28a8856ed0a6430
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-14 11:13:45 +0900

    delete :: stat domain 삭제

--- src/main/java/team/startup/expo/domain/stat/service/GetStandardStatsService.java
+++
@@ -1,7 +0,0 @@
-package team.startup.expo.domain.stat.service;
-
-import team.startup.expo.domain.stat.presentation.dto.response.StandardParticipantStatsResponse;
-
-public interface GetStandardStatsService {
-    StandardParticipantStatsResponse execute(String expoId);
-}
