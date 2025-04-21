7f9c4ee162dcd7d188727668a151d9692423fedd
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-21 21:02:11 +0900

    delete :: TimeZone 설정 삭제

--- src/main/java/team/startup/expo/ExpoApplication.java
+++ src/main/java/team/startup/expo/ExpoApplication.java
@@ -1,24 +1,16 @@
 package team.startup.expo;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.util.TimeZone;
-
 @EnableAsync
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableCaching
 public class ExpoApplication {
-
-	@PostConstruct
-	public void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpoApplication.class, args);
