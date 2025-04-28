91ab700f4442ca826a8de9ec30ba3ffe809df741
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-28 13:53:25 +0900

    delete :: 불필요한 클래스 삭제

--- src/main/java/team/startup/expo/domain/attendance/repository/LeaveManagerRepository.java
+++
@@ -1,7 +0,0 @@
-package team.startup.expo.domain.attendance.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import team.startup.expo.domain.attendance.entity.LeaveManager;
-
-public interface LeaveManagerRepository extends CrudRepository<LeaveManager, String> {
-}
