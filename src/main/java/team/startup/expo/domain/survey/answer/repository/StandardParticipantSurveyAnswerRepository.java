9449548ab93acc188294ee4578412118affb3a20
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-04-28 17:46:20 +0900

    add :: findStandardParticipantSurveyAnswersByStandardParticipantIds query method

--- src/main/java/team/startup/expo/domain/survey/answer/repository/StandardParticipantSurveyAnswerRepository.java
+++ src/main/java/team/startup/expo/domain/survey/answer/repository/StandardParticipantSurveyAnswerRepository.java
@@ -1,9 +1,14 @@
 package team.startup.expo.domain.survey.answer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import team.startup.expo.domain.participant.entity.StandardParticipant;
 import team.startup.expo.domain.survey.answer.entity.StandardParticipantSurveyAnswer;
 
+import java.util.List;
+
 public interface StandardParticipantSurveyAnswerRepository extends JpaRepository<StandardParticipantSurveyAnswer, Long> {
     Boolean existsByStandardParticipant(StandardParticipant standardParticipant);
+    @Query("SELECT spsa FROM StandardParticipantSurveyAnswer spsa WHERE spsa.standardParticipant.id IN :standardParticipantIds")
+    List<StandardParticipantSurveyAnswer> findStandardParticipantSurveyAnswersByStandardParticipantIds(List<Long> standardParticipantIds);
 }
