c1937f89539ffb6f927e1c782725959951014a5a
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-05-01 16:14:58 +0900

    update :: 버킷 환경변수 분리

--- src/main/java/team/startup/expo/global/thirdparty/aws/S3Util.java
+++ src/main/java/team/startup/expo/global/thirdparty/aws/S3Util.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import team.startup.expo.global.thirdparty.aws.exception.FileExtensionInvalidException;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,12 +19,15 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3Util {
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    @Value("${cloud.aws.s3.image-bucket}")
+    private String imageBucket;
+
+    @Value("${cloud.aws.s3.qr-bucket}")
+    private String qrBucket;
 
     private final AmazonS3 amazonS3;
 
-    public String imageUpload(MultipartFile image) {
+    public String upload(MultipartFile image) {
         try {
             List<String> list = List.of("jpg", "jpeg", "png");
 
@@ -35,22 +41,43 @@ public class S3Util {
             if (list.stream().noneMatch(it -> it.equals(extension)))
                 throw new FileExtensionInvalidException();
 
-            return upload(image);
+            String profileName = imageBucket + "/" + UUID.randomUUID() + image.getOriginalFilename();
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(image.getInputStream().available());
+            amazonS3.putObject(imageBucket, profileName, image.getInputStream(), metadata);
+            return amazonS3.getUrl(imageBucket, profileName).toString();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private String upload(MultipartFile file) throws IOException {
-        String profileName = bucket + "/" + UUID.randomUUID() + file.getOriginalFilename();
+    public String qrUpload(File image) throws IOException {
+        String filename = image.getName();
+
+        String[] splitFile = filename.split("\\.");
+
+        if (splitFile.length != 2)
+            throw new FileExtensionInvalidException();
+
+        String extension = splitFile[1].toLowerCase();
+        List<String> allowedExtensions = List.of("jpg", "jpeg", "png");
+
+        if (allowedExtensions.stream().noneMatch(it -> it.equals(extension)))
+            throw new FileExtensionInvalidException();
+
+        String profileName = qrBucket + "/" + UUID.randomUUID() + filename;
         ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getInputStream().available());
-        amazonS3.putObject(bucket, profileName, file.getInputStream(), metadata);
-        return amazonS3.getUrl(bucket, profileName).toString();
+        metadata.setContentLength(Files.size(image.toPath()));
+        metadata.setContentType("image/" + extension);
+
+        amazonS3.putObject(qrBucket, profileName, new FileInputStream(image), metadata);
+        return amazonS3.getUrl(qrBucket, profileName).toString();
     }
+
 
     public void deleteImage(String url) {
         String key = url.split("com/")[1];
-        amazonS3.deleteObject(bucket, key);
+        amazonS3.deleteObject(imageBucket, key);
     }
 }
