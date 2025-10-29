6927f8ef05e51aece48b42928df0762d40dbf88d
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-29 13:46:39 +0900

    update :: RegistrationType 추가

--- src/main/java/team/startup/expo/domain/form/service/GetFormService.java
+++ src/main/java/team/startup/expo/domain/form/service/GetFormService.java
@@ -1,8 +1,9 @@
 package team.startup.expo.domain.form.service;
 
 import team.startup.expo.domain.form.entity.ParticipationType;
+import team.startup.expo.domain.form.entity.RegistrationType;
 import team.startup.expo.domain.form.presentation.dto.response.GetFormResponseDto;
 
 public interface GetFormService {
-    GetFormResponseDto execute(String expoId, ParticipationType participationType);
+    GetFormResponseDto execute(String expoId, ParticipationType participationType, RegistrationType registrationType);
 }
