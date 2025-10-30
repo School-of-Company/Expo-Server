d15fbb2c37a438ea17bfb718ce725782981fdfad
seungchan <s24041@gsm.hs.kr>
2025-10-30 14:46:13 +0900

    add :: mongoDB 마이그레이션에 따른 로직 추가

--- src/main/java/team/startup/expo/domain/survey/management/service/impl/UpdateSurveyServiceImpl.java
+++ src/main/java/team/startup/expo/domain/survey/management/service/impl/UpdateSurveyServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.cache.annotation.CacheEvict;
 import team.startup.expo.domain.expo.entity.Expo;
 import team.startup.expo.domain.expo.exception.NotFoundExpoException;
 import team.startup.expo.domain.expo.repository.ExpoRepository;
+import team.startup.expo.domain.mongo.entity.DynamicSurveyJsonDoc;
+import team.startup.expo.domain.mongo.repository.DynamicSurveyJsonRepository;
 import team.startup.expo.domain.survey.management.entity.DynamicSurvey;
 import team.startup.expo.domain.survey.management.entity.Survey;
 import team.startup.expo.domain.survey.management.exception.NotFoundSurveyException;
@@ -23,6 +25,7 @@ public class UpdateSurveyServiceImpl implements UpdateSurveyService {
     private final SurveyRepository surveyRepository;
     private final DynamicSurveyRepository dynamicSurveyRepository;
     private final ExpoRepository expoRepository;
+    private final DynamicSurveyJsonRepository dynamicSurveyJsonRepository;
 
     @CacheEvict(key = "#expoId + '_' + #dto.participationType", cacheManager = "cacheManager")
     public void execute(String expoId, SurveyRequestDto dto) {
@@ -33,20 +36,24 @@ public class UpdateSurveyServiceImpl implements UpdateSurveyService {
                 .orElseThrow(NotFoundSurveyException::new);
 
         survey.update(dto.getInformationText());
-        dynamicSurveyRepository.deleteBySurvey(survey);
         dto.getDynamicSurveyRequestDto().forEach(dynamicSurveyRequestDto -> saveDynamicSurvey(dynamicSurveyRequestDto, survey));
     }
 
     private void saveDynamicSurvey(SurveyRequestDto.DynamicSurveyRequestDto dto, Survey survey) {
         DynamicSurvey dynamicSurvey = DynamicSurvey.builder()
+                .surveyId(survey.getId())
                 .title(dto.getTitle())
-                .jsonData(dto.getJsonData())
                 .formType(dto.getFormType())
                 .requiredStatus(dto.getRequiredStatus())
-                .otherJson(dto.getOtherJson())
-                .survey(survey)
                 .build();
+        dynamicSurvey = dynamicSurveyRepository.save(dynamicSurvey);
 
-        dynamicSurveyRepository.save(dynamicSurvey);
+        DynamicSurveyJsonDoc doc = new DynamicSurveyJsonDoc(
+                null,
+                dynamicSurvey.getId(),
+                dto.getJsonData(),
+                dto.getOtherJson()
+        );
+        dynamicSurveyJsonRepository.save(doc);
     }
 }
