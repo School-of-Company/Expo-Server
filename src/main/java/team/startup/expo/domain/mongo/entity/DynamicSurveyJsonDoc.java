c9ddbe5a559b1d31b47fe167f7a74f482728138c
seungchan <s24041@gsm.hs.kr>
2025-10-30 14:42:39 +0900

    add :: DynamicSurveyJsonDoc 추가

---
+++ src/main/java/team/startup/expo/domain/mongo/entity/DynamicSurveyJsonDoc.java
@@ -0,0 +1,27 @@
+package team.startup.expo.domain.mongo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "dynamic_survey_json")
+public class DynamicSurveyJsonDoc {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private Long recordId;
+
+    private Map<String, Object> jsonData;
+
+    private Map<String, Object> otherJson;
+}
