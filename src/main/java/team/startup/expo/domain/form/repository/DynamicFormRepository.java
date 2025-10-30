2ad449259260e3c49f9d62b66c636a1f500ca38d
seungchan <s24041@gsm.hs.kr>
2025-10-30 14:41:29 +0900

    update :: 메서드, 파라미터 변경

--- src/main/java/team/startup/expo/domain/form/repository/DynamicFormRepository.java
+++ src/main/java/team/startup/expo/domain/form/repository/DynamicFormRepository.java
@@ -2,11 +2,10 @@ package team.startup.expo.domain.form.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.expo.domain.form.entity.DynamicForm;
-import team.startup.expo.domain.form.entity.Form;
 
 import java.util.List;
 
 public interface DynamicFormRepository extends JpaRepository<DynamicForm, Long> {
-    void deleteByForm(Form form);
-    List<DynamicForm> findByForm(Form form);
+    void deleteByFormId(Long formId);
+    List<DynamicForm> findByFormId(Long formId);
 }
