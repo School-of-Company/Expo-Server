da731149ee6d1ec01fd53e33453be3823626b063
seungchan <s24041@gsm.hs.kr>
2025-10-30 14:39:03 +0900

    add :: mongoDB 마이그레이션에 따른 로직 수정

--- src/main/java/team/startup/expo/domain/survey/management/service/impl/DeleteSurveyServiceImpl.java
+++ src/main/java/team/startup/expo/domain/survey/management/service/impl/DeleteSurveyServiceImpl.java
@@ -7,12 +7,16 @@ import team.startup.expo.domain.expo.entity.Expo;
 import team.startup.expo.domain.expo.exception.NotFoundExpoException;
 import team.startup.expo.domain.expo.repository.ExpoRepository;
 import team.startup.expo.domain.form.entity.ParticipationType;
+import team.startup.expo.domain.survey.management.entity.DynamicSurvey;
 import team.startup.expo.domain.survey.management.entity.Survey;
 import team.startup.expo.domain.survey.management.exception.NotFoundSurveyException;
 import team.startup.expo.domain.survey.management.repository.DynamicSurveyRepository;
 import team.startup.expo.domain.survey.management.repository.SurveyRepository;
 import team.startup.expo.domain.survey.management.service.DeleteSurveyService;
+import team.startup.expo.domain.mongo.repository.DynamicSurveyJsonRepository;
 import team.startup.expo.global.annotation.TransactionService;
+
+import java.util.List;
 
 @TransactionService
 @RequiredArgsConstructor
@@ -22,6 +26,7 @@ public class DeleteSurveyServiceImpl implements DeleteSurveyService {
     private final SurveyRepository surveyRepository;
     private final DynamicSurveyRepository dynamicSurveyRepository;
     private final ExpoRepository expoRepository;
+    private final DynamicSurveyJsonRepository dynamicSurveyJsonRepository;
 
     @CacheEvict(key = "#expoId + '_' + #participationType", cacheManager = "cacheManager")
     public void execute(String expoId, ParticipationType participationType) {
@@ -31,7 +36,11 @@ public class DeleteSurveyServiceImpl implements DeleteSurveyService {
         Survey survey = surveyRepository.findByExpoAndParticipationType(expo, participationType)
                 .orElseThrow(NotFoundSurveyException::new);
 
-        dynamicSurveyRepository.deleteBySurvey(survey);
+        List<DynamicSurvey> dynamicSurveys = dynamicSurveyRepository.findBySurveyId(survey.getId());
+        for (DynamicSurvey ds : dynamicSurveys) {
+            dynamicSurveyJsonRepository.deleteByRecordId(ds.getId());
+        }
+        dynamicSurveyRepository.deleteAll(dynamicSurveys);
         surveyRepository.delete(survey);
     }
 }
