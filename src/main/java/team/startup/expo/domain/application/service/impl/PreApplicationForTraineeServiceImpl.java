677245c5ee3a5b487d2b8c97a33198f5e23efdbb
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-03 09:02:46 +0900

    update :: 엔티티 생성 시 applicationDate 값 추가

--- src/main/java/team/startup/expo/domain/application/service/impl/PreApplicationForTraineeServiceImpl.java
+++ src/main/java/team/startup/expo/domain/application/service/impl/PreApplicationForTraineeServiceImpl.java
@@ -25,6 +25,7 @@ import team.startup.expo.global.date.DateUtil;
 import team.startup.expo.global.exception.ErrorCode;
 import team.startup.expo.global.exception.GlobalException;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @TransactionService
@@ -74,6 +75,7 @@ public class PreApplicationForTraineeServiceImpl implements PreApplicationForTra
                         .applicationType(ApplicationType.PRE)
                         .personalInformationStatus(dto.getPersonalInformationStatus())
                         .expo(expo)
+                        .applicationDate(LocalDateTime.now())
                         .build());
 
         traineeRepository.save(trainee);
