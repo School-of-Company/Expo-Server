75b5db13a99add7827e8d3a174e5fc2a2ee83518
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-26 23:21:04 +0900

    update :: koreanName 필드 추가

--- src/main/java/team/startup/expo/domain/trainee/entity/ApplicationType.java
+++ src/main/java/team/startup/expo/domain/trainee/entity/ApplicationType.java
@@ -1,6 +1,16 @@
 package team.startup.expo.domain.trainee.entity;
 
 public enum ApplicationType {
-    PRE,
-    FIELD
+    PRE("사전 등록"),
+    FIELD("현장 등록");
+
+    private final String koreanName;
+
+    ApplicationType(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
 }
